1bd62791b11b04173e8dd9671a19c0b116e4fe2a
Jordan <jormmiller@gmail.com>
2024-03-30 15:02:40 -0400

    added required packes, added questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,48 @@
-// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generator = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        name: 'title',
+        message: 'Enter project title:'
+    },
+    {
+        name: 'description',
+        message: 'Enter project description:'
+    },
+    {
+        name: 'installation',
+        message: 'Enter installation instructions:'
+    },
+    {
+        name: 'usage',
+        message: 'Enter usage information:'
+    },
+    {
+        name: 'contribution',
+        message: 'Enter contribution guidelines:'
+    },
+    {
+        name: 'test',
+        message: 'Enter test instructions:'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license:',
+        choices: ['MIT', 'Apache', 'GPL', 'Mozilla', 'BSD 3']
+    },
+    {
+        name: 'github',
+        message: 'Enter your Github username:'
+    },
+    {
+        name: 'email',
+        message: 'Enter your email address:'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
