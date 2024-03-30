a927895e8f0f7dc870e0fa0de2b5833288361fd6
Jordan <jormmiller@gmail.com>
2024-03-30 15:04:30 -0400

    added write file function

--- Develop/index.js
+++ Develop/index.js
@@ -2,7 +2,6 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const generator = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
 const questions = [
     {
         name: 'title',
@@ -45,10 +44,18 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log('README.md file created');
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
